2e3ac2949ed674d0bedbce51e02e309a1193f475
Ewan Burns <ewan@beezup.com>
2019-02-21 17:47:15 +0100

    Adding index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as internalWinston from "./bytehappens-winston";
+import * as internalConsole from "./bytehappens-console";
+import * as internalTelegram from "./bytehappens-telegram";
+import * as internalMongoDb from "./bytehappens-mongodb";
+
+export import loggingWinston = internalWinston;
+export import loggingConsole = internalConsole;
+export import loggingTelegram = internalTelegram;
+export import loggingMongoDb = internalMongoDb;
