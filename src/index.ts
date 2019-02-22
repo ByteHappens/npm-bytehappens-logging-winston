48f80865d6e94c3bef16769d742da30bd96ee574
Ewan Burns <ewan@beezup.com>
2019-02-22 16:40:13 +0100

    Moving code around and changing namspaces

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import * as internalWinston from "./bytehappens-winston";
-import * as internalConsole from "./bytehappens-console";
-import * as internalTelegram from "./bytehappens-telegram";
-import * as internalMongoDb from "./bytehappens-mongodb";
+import * as internalCore from "./core";
+import * as internalConsole from "./console";
+import * as internalTelegram from "./telegram";
+import * as internalMongoDb from "./mongodb";
 
-export import loggingWinston = internalWinston;
-export import loggingConsole = internalConsole;
-export import loggingTelegram = internalTelegram;
-export import loggingMongoDb = internalMongoDb;
+export namespace loggingWinston {
+  //  SCK: Not too happy about this, will change when i figure how to put it at root
+  export import core = internalCore;
+
+  export import console = internalConsole;
+  export import telegram = internalTelegram;
+  export import mongodb = internalMongoDb;
+}
